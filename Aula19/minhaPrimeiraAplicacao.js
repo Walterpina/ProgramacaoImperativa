4c935bbfda0705bc5e5c6720e9e3cd975b6b711c
Walterpina <walterrpina@gmail.com>
2021-09-23 01:38:30 -0300

    Ajustado e melhorado o exerc "minha primeira app

--- Aula19/minhaPrimeiraAplicacao.js
+++ Aula19/minhaPrimeiraAplicacao.js
@@ -1,106 +1,166 @@
-let SexoPessoas = function(sexo, altura) {
-  this.sexo = sexo;
-  this.altura = altura;
-};
+/**
+ * MINHA PRIMEIRA APLICAÇÃO
+ * 
+ * Tem-se um conjunto de dados contendo a altura e o sexo (M ou F) de 15 pessoas. Faça um programa que calcule e escreva:	
+    a maior e a menor altura do grupo;
+    a média de altura das mulheres;
+    o número de homens.
+ */
+function calcular() {//nesta funçção eu não coloquei um elemento pq tudo está dentro dela
+  //do array "pessoas" aos cálculos que deverão ser realizados mais abaixo
+  let pessoas = [
+    {
+      sexo: "M",
+      altura: 1.80,
+    },
+    {
+      sexo: "F",
+      altura: 1.63,
+    },
+    {
+      sexo: "M",
+      altura: 1.75,
+    },
+    {
+      sexo: "M",
+      altura: 1.83,
+    },
+    {
+      sexo: "F",
+      altura: 1.73,
+    },
+    {
+      sexo: "F",
+      altura: 1.76,
+    },
+    {
+      sexo: "M",
+      altura: 1.79,
+    },
+    {
+      sexo: "F",
+      altura: 1.64,
+    },
+    {
+      sexo: "M",
+      altura: 1.89,
+    },
+    {
+      sexo: "F",
+      altura: 1.81,
+    },
+    {
+      sexo: "M",
+      altura: 1.78,
+    },
+    {
+      sexo: "M",
+      altura: 1.65,
+    },
+    {
+      sexo: "M",
+      altura: 1.81,
+    },
+    {
+      sexo: "M",
+      altura: 1.77,
+    },
+    {
+      sexo: "M",
+      altura: 1.82,
+    },
+    {
+      sexo: "F",
+      altura: 1.71,
+    }
+  ];
 
-let pessoas = [
-  {
-    "sexo": "M",
-    "altura": 1.80,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.63,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.75,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.83,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.73,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.76,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.79,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.64,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.89,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.81,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.78,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.65,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.81,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.77,
-  },
-  {
-    "sexo": "M",
-    "altura": 1.82,
-  },
-  {
-    "sexo": "F",
-    "altura": 1.71
+  //"Calcula" o menor e maior números dos dados
+  pessoas.sort(function(a, b) { //esta função está colocando em ordem os valores do menor para o maior
+    return a.altura - b.altura;// a ordem é crescente como está determinado aqui em a - b, ou seja,
+  }); //o primeiro número(índice) "menos" o segundo número(índice)
+
+  //Média de altura das mulheres
+  //aqui foram criadas as variáveis que serão necessárias para a aplciação
+  let alturaMulheres = 0;
+  let qtdMulheres = 0;
+  let qtdHomens = 0;
+
+  //aqui está o loop com o array pessoas que fará a contagem das informações que precisamos
+  for(let i = 0; i < pessoas.length; i++) {
+    if(pessoas[i].sexo === "F") {
+      qtdMulheres += 1;//aqui é feita a contagem da quantidade das mulheres
+      //console.log(qtdMulheres);
+      alturaMulheres += pessoas[i].altura;//aqui é feita a contagem da altura das mulheres
+      //console.log(alturaMulheres);
+    } else {
+      qtdHomens += 1; //aqui é feita a contagem da quantidade dos homens
+    }
   }
+
+  let mediaMulheres = (alturaMulheres / qtdMulheres).toFixed(2); //aqui é calculada a média da altura das mulheres
+
+  console.log('A menor altura registrada é: ' + pessoas[0].altura); //aqui eu seleciono o menor valor (pegando do índice 0)
+  console.log('A maior altura registrada é: ' + pessoas[15].altura); //aqui eu seleciono o maior valor (pegando do índice 15)
+  console.log('A média da altura das mulheres é de: ' + mediaMulheres);
+  console.log('A quantidade de homens é ' + qtdHomens);
+}
+
+calcular(); //aqui é pedido para que a funçaõ seja executada.
+
+console.log('------------------------------------------------------')
+/**
+ * Cada espectador de um cinema respondeu a um questionário no qual constava sua idade e a 
+ * sua opinião em relação ao filme: ótimo - 3, bom - 2, regular -1.
+    Faça um programa que receba a idade e a opinião de 15 espectadores, calcule e imprima:
+      a média das idades das pessoas que responderam ótimo;
+      a quantidade de pessoas que responderam regular;
+      a porcentagem de pessoas que responderam bom entre todos os espectadores analisados.
+ */
+
+//criado o objeto fora da função.
+let cinefalos = [
+  {idade: 18, opiniao: 2},
+  {idade: 30, opiniao: 3},
+  {idade: 15, opiniao: 3},
+  {idade: 43, opiniao: 1},
+  {idade: 19, opiniao: 2},
+  {idade: 20, opiniao: 2},
+  {idade: 32, opiniao: 3},
+  {idade: 17, opiniao: 3},
+  {idade: 27, opiniao: 1},
+  {idade: 28, opiniao: 3},
+  {idade: 38, opiniao: 1},
+  {idade: 39, opiniao: 1},
+  {idade: 40, opiniao: 3},
+  {idade: 39, opiniao: 3},
+  {idade: 19, opiniao: 1},
 ];
 
-let listaPessoas = [];
-for(let cont = 0; cont < pessoas.length; cont++) {
-  listaPessoas.push(new SexoPessoas(pessoas[cont].sexo, pessoas[cont].altura));
-};
-//console.log(listaPessoas);
-
-//primeiro ponto - for
-//segundo ponto - maior altura e menor altura
-
-let Altura = {
-  pessoas: listaPessoas,
-  maiorAltura: function(altura){
-    let maior = 0;
-    for(index in pessoas) {
-      if(maior < pessoas[index].altura) {
-        maior = pessoas[index].altura;
-      }
-      return maior;
+function calcularOpiniao(arr) { //o elemento que a função irá fazer a leitura eu chamei de "arr"
+  //console.log(arr)
+  let somaIdadeOtimo = 0;
+  let quantidadeOtimo = 0;
+  let quantidadeBom = 0;
+  let quantidadeRegular = 0;
+  //aqui é criado o loop que fará a leitura do array cinefalos
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].opiniao == 3) {//aqui eu defino a pesquisa pela nota 3 e calculo
+      somaIdadeOtimo += arr[i].idade; //soma das idades deste voto
+      quantidadeOtimo += 1; //soma de quantidade votaram 3
+    } else if(arr[i].opiniao == 2) {
+      quantidadeBom += 1; //soma de quantidade da nota 2
+    } else {
+      quantidadeRegular += 1;//soma da quantidade de nota 1
     }
-  },
-  menorAltura: function(altura){
-    let menor = 0;
-    for(i in pessoas) {
-      if(menor > pessoas[i].altura) {
-        menor = pessoas[i].altura;
-      }
-    }
-    return menor;
   }
-};
-console.log(Altura.maiorAltura());
-console.log(Altura.menorAltura());
+  // console.log(somaIdadeOtimo);
+  // console.log(quantidadeRegular);
 
+  let mediaIdade = (somaIdadeOtimo / quantidadeOtimo).toFixed(2);//calculo da média da idade do voto de ótimo
+  let percentagem = ((quantidadeBom / arr.length) * 100).toFixed(2); //calculo do percentual
 
+  console.log('A média das idade que responderam ótimo é ' + mediaIdade);
+  console.log('A percentagem de peessoas que responderam bom entre todos os espectadores é ' + percentagem + '%.');
+}
+calcularOpiniao(cinefalos);//aqui é executada a função com o elemento "cinefalos" 
